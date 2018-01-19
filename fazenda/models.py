d9741cbc2bfe965d71934625b4d7cae394499d16
jonas <ferreirajonasss@gmail.com>
2018-01-19 21:07:53 -0200

    modelagem do banco de dados

--- fazenda/models.py
+++ fazenda/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Fazenda(models.Model):
+    
+    cnpj = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=50)
+    endereco = models.TextField()
+
+
+class GestorFazenda(models.Model):
+    
+    usuario = models.OneToOneField(User)
+    fazenda = models.ForeignKey(Fazenda)
+
+
+class Gado(models.Model):
+    
+    numero_brinco = models.CharField(max_length=50)
+    peso = models.FloatField()
+    especificacoes = models.TextField(null=True, blank=True)
+    fazenda = models.ForeignKey(Fazenda)
--- jetbov/settings.py
+++ jetbov/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jetbov',
+    'fazenda',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
